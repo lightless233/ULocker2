33117201747f0fd9c9bd61e8cf7b073f3a196e97
LightSoulSoul <lightless@foxmail.com>
2014-06-30 19:29:04 +0800

    finished OpenFileButton

--- ULocker2/Form1.Designer.cs
+++ ULocker2/Form1.Designer.cs
@@ -30,30 +30,30 @@
 		{
 			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
 			this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.帮助ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.帮助ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.帮助ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
 			this.关于ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.groupBox1 = new System.Windows.Forms.GroupBox();
-			this.textBox1 = new System.Windows.Forms.TextBox();
-			this.button1 = new System.Windows.Forms.Button();
+			this.buttonSelectFile = new System.Windows.Forms.Button();
+			this.textFilePath = new System.Windows.Forms.TextBox();
 			this.groupBox2 = new System.Windows.Forms.GroupBox();
 			this.comboBox1 = new System.Windows.Forms.ComboBox();
 			this.groupBox3 = new System.Windows.Forms.GroupBox();
-			this.groupBox4 = new System.Windows.Forms.GroupBox();
-			this.comboBox2 = new System.Windows.Forms.ComboBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button5 = new System.Windows.Forms.Button();
 			this.comboBox3 = new System.Windows.Forms.ComboBox();
-			this.textBox2 = new System.Windows.Forms.TextBox();
 			this.textBox3 = new System.Windows.Forms.TextBox();
-			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.comboBoxEncryptionAlgorithm = new System.Windows.Forms.ComboBox();
+			this.groupBox4 = new System.Windows.Forms.GroupBox();
 			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
 			this.button2 = new System.Windows.Forms.Button();
 			this.button4 = new System.Windows.Forms.Button();
-			this.button5 = new System.Windows.Forms.Button();
-			this.label1 = new System.Windows.Forms.Label();
-			this.label2 = new System.Windows.Forms.Label();
-			this.label3 = new System.Windows.Forms.Label();
-			this.label4 = new System.Windows.Forms.Label();
 			this.menuStrip1.SuspendLayout();
 			this.groupBox1.SuspendLayout();
 			this.groupBox2.SuspendLayout();
@@ -80,6 +80,12 @@
 			this.文件ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
 			this.文件ToolStripMenuItem.Text = "文件";
 			// 
+			// 退出ToolStripMenuItem
+			// 
+			this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+			this.退出ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+			this.退出ToolStripMenuItem.Text = "退出";
+			// 
 			// 帮助ToolStripMenuItem
 			// 
 			this.帮助ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
@@ -89,28 +95,22 @@
 			this.帮助ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
 			this.帮助ToolStripMenuItem.Text = "帮助";
 			// 
-			// 退出ToolStripMenuItem
-			// 
-			this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
-			this.退出ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-			this.退出ToolStripMenuItem.Text = "退出";
-			// 
 			// 帮助ToolStripMenuItem1
 			// 
 			this.帮助ToolStripMenuItem1.Name = "帮助ToolStripMenuItem1";
-			this.帮助ToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+			this.帮助ToolStripMenuItem1.Size = new System.Drawing.Size(100, 22);
 			this.帮助ToolStripMenuItem1.Text = "帮助";
 			// 
 			// 关于ToolStripMenuItem
 			// 
 			this.关于ToolStripMenuItem.Name = "关于ToolStripMenuItem";
-			this.关于ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.关于ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
 			this.关于ToolStripMenuItem.Text = "关于";
 			// 
 			// groupBox1
 			// 
-			this.groupBox1.Controls.Add(this.button1);
-			this.groupBox1.Controls.Add(this.textBox1);
+			this.groupBox1.Controls.Add(this.buttonSelectFile);
+			this.groupBox1.Controls.Add(this.textFilePath);
 			this.groupBox1.Location = new System.Drawing.Point(12, 28);
 			this.groupBox1.Name = "groupBox1";
 			this.groupBox1.Size = new System.Drawing.Size(538, 52);
@@ -118,21 +118,22 @@
 			this.groupBox1.TabStop = false;
 			this.groupBox1.Text = "选择需要操作的文件";
 			// 
-			// textBox1
+			// buttonSelectFile
 			// 
-			this.textBox1.Location = new System.Drawing.Point(6, 20);
-			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(375, 21);
-			this.textBox1.TabIndex = 0;
+			this.buttonSelectFile.Location = new System.Drawing.Point(398, 18);
+			this.buttonSelectFile.Name = "buttonSelectFile";
+			this.buttonSelectFile.Size = new System.Drawing.Size(119, 23);
+			this.buttonSelectFile.TabIndex = 1;
+			this.buttonSelectFile.Text = "选择文件...";
+			this.buttonSelectFile.UseVisualStyleBackColor = true;
+			this.buttonSelectFile.Click += new System.EventHandler(this.buttonSelectFile_Click);
 			// 
-			// button1
+			// textFilePath
 			// 
-			this.button1.Location = new System.Drawing.Point(398, 18);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(119, 23);
-			this.button1.TabIndex = 1;
-			this.button1.Text = "选择文件...";
-			this.button1.UseVisualStyleBackColor = true;
+			this.textFilePath.Location = new System.Drawing.Point(6, 20);
+			this.textFilePath.Name = "textFilePath";
+			this.textFilePath.Size = new System.Drawing.Size(375, 21);
+			this.textFilePath.TabIndex = 0;
 			// 
 			// groupBox2
 			// 
@@ -163,13 +164,95 @@
 			this.groupBox3.Controls.Add(this.comboBox3);
 			this.groupBox3.Controls.Add(this.textBox3);
 			this.groupBox3.Controls.Add(this.textBox2);
-			this.groupBox3.Controls.Add(this.comboBox2);
+			this.groupBox3.Controls.Add(this.comboBoxEncryptionAlgorithm);
 			this.groupBox3.Location = new System.Drawing.Point(12, 145);
 			this.groupBox3.Name = "groupBox3";
 			this.groupBox3.Size = new System.Drawing.Size(409, 168);
 			this.groupBox3.TabIndex = 3;
 			this.groupBox3.TabStop = false;
 			this.groupBox3.Text = "加解密设置";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(12, 126);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(77, 12);
+			this.label4.TabIndex = 6;
+			this.label4.Text = "自定义密钥：";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(13, 92);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(77, 12);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "加解密算法：";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(6, 58);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(89, 12);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "文件共享方式：";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(18, 23);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(53, 12);
+			this.label1.TabIndex = 6;
+			this.label1.Text = "用户名：";
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(283, 18);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(120, 23);
+			this.button5.TabIndex = 5;
+			this.button5.Text = "获取我的用户组";
+			this.button5.UseVisualStyleBackColor = true;
+			// 
+			// comboBox3
+			// 
+			this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox3.FormattingEnabled = true;
+			this.comboBox3.Location = new System.Drawing.Point(99, 55);
+			this.comboBox3.Name = "comboBox3";
+			this.comboBox3.Size = new System.Drawing.Size(304, 20);
+			this.comboBox3.TabIndex = 0;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(99, 123);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(304, 21);
+			this.textBox3.TabIndex = 0;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(99, 20);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(172, 21);
+			this.textBox2.TabIndex = 0;
+			// 
+			// comboBoxEncryptionAlgorithm
+			// 
+			this.comboBoxEncryptionAlgorithm.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBoxEncryptionAlgorithm.FormattingEnabled = true;
+			this.comboBoxEncryptionAlgorithm.Items.AddRange(new object[] {
+            "AES - 高级加密标准 (默认，推荐算法)",
+            "DES - 数据加密算法 (适合文件保密性不高的文件)",
+            "TripleDES - 3层数据加密算法 (比DES安全性较高)",
+            "RC2 - Ron\'s Code (速度快，适合大文件)"});
+			this.comboBoxEncryptionAlgorithm.Location = new System.Drawing.Point(99, 89);
+			this.comboBoxEncryptionAlgorithm.Name = "comboBoxEncryptionAlgorithm";
+			this.comboBoxEncryptionAlgorithm.Size = new System.Drawing.Size(304, 20);
+			this.comboBoxEncryptionAlgorithm.TabIndex = 0;
 			// 
 			// groupBox4
 			// 
@@ -182,42 +265,16 @@
 			this.groupBox4.TabStop = false;
 			this.groupBox4.Text = "功能区";
 			// 
-			// comboBox2
+			// radioButton2
 			// 
-			this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-			this.comboBox2.FormattingEnabled = true;
-			this.comboBox2.Items.AddRange(new object[] {
-            "AES - 高级加密标准 (默认，推荐算法)",
-            "DES - 数据加密算法 (适合文件保密性不高的文件)",
-            "TripleDES - 3层数据加密算法 (比DES安全性较高)",
-            "RC2 - Ron\'s Code (速度快，适合大文件)"});
-			this.comboBox2.Location = new System.Drawing.Point(99, 89);
-			this.comboBox2.Name = "comboBox2";
-			this.comboBox2.Size = new System.Drawing.Size(304, 20);
-			this.comboBox2.TabIndex = 0;
-			// 
-			// comboBox3
-			// 
-			this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-			this.comboBox3.FormattingEnabled = true;
-			this.comboBox3.Location = new System.Drawing.Point(99, 55);
-			this.comboBox3.Name = "comboBox3";
-			this.comboBox3.Size = new System.Drawing.Size(304, 20);
-			this.comboBox3.TabIndex = 0;
-			// 
-			// textBox2
-			// 
-			this.textBox2.Location = new System.Drawing.Point(99, 20);
-			this.textBox2.Name = "textBox2";
-			this.textBox2.Size = new System.Drawing.Size(172, 21);
-			this.textBox2.TabIndex = 0;
-			// 
-			// textBox3
-			// 
-			this.textBox3.Location = new System.Drawing.Point(99, 123);
-			this.textBox3.Name = "textBox3";
-			this.textBox3.Size = new System.Drawing.Size(304, 21);
-			this.textBox3.TabIndex = 0;
+			this.radioButton2.AutoSize = true;
+			this.radioButton2.Location = new System.Drawing.Point(35, 43);
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.Size = new System.Drawing.Size(47, 16);
+			this.radioButton2.TabIndex = 0;
+			this.radioButton2.TabStop = true;
+			this.radioButton2.Text = "解密";
+			this.radioButton2.UseVisualStyleBackColor = true;
 			// 
 			// radioButton1
 			// 
@@ -229,17 +286,6 @@
 			this.radioButton1.TabStop = true;
 			this.radioButton1.Text = "加密";
 			this.radioButton1.UseVisualStyleBackColor = true;
-			// 
-			// radioButton2
-			// 
-			this.radioButton2.AutoSize = true;
-			this.radioButton2.Location = new System.Drawing.Point(35, 43);
-			this.radioButton2.Name = "radioButton2";
-			this.radioButton2.Size = new System.Drawing.Size(47, 16);
-			this.radioButton2.TabIndex = 0;
-			this.radioButton2.TabStop = true;
-			this.radioButton2.Text = "解密";
-			this.radioButton2.UseVisualStyleBackColor = true;
 			// 
 			// button2
 			// 
@@ -258,51 +304,6 @@
 			this.button4.TabIndex = 5;
 			this.button4.Text = "退出程序";
 			this.button4.UseVisualStyleBackColor = true;
-			// 
-			// button5
-			// 
-			this.button5.Location = new System.Drawing.Point(283, 18);
-			this.button5.Name = "button5";
-			this.button5.Size = new System.Drawing.Size(120, 23);
-			this.button5.TabIndex = 5;
-			this.button5.Text = "获取我的用户组";
-			this.button5.UseVisualStyleBackColor = true;
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(18, 23);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(53, 12);
-			this.label1.TabIndex = 6;
-			this.label1.Text = "用户名：";
-			// 
-			// label2
-			// 
-			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(6, 58);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(89, 12);
-			this.label2.TabIndex = 6;
-			this.label2.Text = "文件共享方式：";
-			// 
-			// label3
-			// 
-			this.label3.AutoSize = true;
-			this.label3.Location = new System.Drawing.Point(18, 92);
-			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(65, 12);
-			this.label3.TabIndex = 6;
-			this.label3.Text = "加密算法：";
-			// 
-			// label4
-			// 
-			this.label4.AutoSize = true;
-			this.label4.Location = new System.Drawing.Point(12, 126);
-			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(77, 12);
-			this.label4.TabIndex = 6;
-			this.label4.Text = "自定义密钥：";
 			// 
 			// Form1
 			// 
@@ -344,8 +345,8 @@
 		private System.Windows.Forms.ToolStripMenuItem 帮助ToolStripMenuItem1;
 		private System.Windows.Forms.ToolStripMenuItem 关于ToolStripMenuItem;
 		private System.Windows.Forms.GroupBox groupBox1;
-		private System.Windows.Forms.Button button1;
-		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button buttonSelectFile;
+		private System.Windows.Forms.TextBox textFilePath;
 		private System.Windows.Forms.GroupBox groupBox2;
 		private System.Windows.Forms.ComboBox comboBox1;
 		private System.Windows.Forms.GroupBox groupBox3;
@@ -353,7 +354,7 @@
 		private System.Windows.Forms.ComboBox comboBox3;
 		private System.Windows.Forms.TextBox textBox3;
 		private System.Windows.Forms.TextBox textBox2;
-		private System.Windows.Forms.ComboBox comboBox2;
+		private System.Windows.Forms.ComboBox comboBoxEncryptionAlgorithm;
 		private System.Windows.Forms.RadioButton radioButton2;
 		private System.Windows.Forms.RadioButton radioButton1;
 		private System.Windows.Forms.Button button2;
--- ULocker2/Form1.cs
+++ ULocker2/Form1.cs
@@ -5,8 +5,24 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Management;
+using System.Threading;
+using System.Security.Cryptography;
+using System.Net;
+
+/************************************************************************/
+/* 
+ *	Powered By Lightless. 
+ *	
+ *	2014.06.30
+ *
+ *	lightless@foxmail.com
+ *
+/************************************************************************/
+
+
 
 namespace ULocker2
 {
@@ -15,6 +31,57 @@ namespace ULocker2
 		public Form1()
 		{
 			InitializeComponent();
+
+			// 设置新线程，负责进行初始化
+			Thread threadInitApp = new Thread(InitApp);
+			threadInitApp.Start();
 		}
+
+		/************************************************************************/
+		/* 全局变量                                                             */
+		/************************************************************************/
+		// U盘的数量
+		uint gRemoveableDeviceCount = 0;
+
+		// 打开文件按钮
+		//
+		// 状态：完成
+		// 完成时间：2014年6月30日 19:18:04
+		// 最后修改日期：2014年6月30日 19:18:15
+		// 编写者：lightless
+		private void buttonSelectFile_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog openDialog = new OpenFileDialog();
+			openDialog.Title = "请选择需要操作的文件";
+			DialogResult x;
+			try
+			{
+				x = openDialog.ShowDialog();
+			}
+			catch (System.Exception ex)
+			{
+				MessageBox.Show("OpenFileDialog Failed!\r\n" + ex.Message);
+				return;
+			}
+			if (x == DialogResult.OK || x == DialogResult.Yes)
+			{
+				string strFilePath = openDialog.FileName;
+				this.textFilePath.Text = strFilePath;
+			}
+		}
+
+		// 初始化线程函数
+		//
+		// 状态：编写中
+		// 完成时间：null
+		// 最后修改日期：2014年6月30日 19:24:38
+		// 编写者：lightless
+		private void InitApp()
+		{
+			// 设置“加解密算法”combox默认为第一项
+			this.comboBoxEncryptionAlgorithm.SelectedIndex = 0;
+		}
+
+
 	}
 }
