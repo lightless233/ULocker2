5a210ab1f34c056dcb1d6516ed8808352f397994
LightSoulSoul <lightless@foxmail.com>
2014-07-02 14:54:27 +0800

    Registry Form UI finished.

--- ULocker2/RegistryForm.Designer.cs
+++ ULocker2/RegistryForm.Designer.cs
@@ -29,25 +29,169 @@
 		private void InitializeComponent()
 		{
 			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label7 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(158, 95);
+			this.label1.Location = new System.Drawing.Point(15, 9);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(41, 12);
+			this.label1.Size = new System.Drawing.Size(59, 12);
 			this.label1.TabIndex = 0;
-			this.label1.Text = "label1";
+			this.label1.Text = "*用户名：";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(70, 6);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(152, 21);
+			this.textBox1.TabIndex = 1;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(70, 33);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(152, 21);
+			this.textBox2.TabIndex = 3;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(27, 36);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(47, 12);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "*密码：";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(70, 60);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(152, 21);
+			this.textBox3.TabIndex = 5;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(3, 63);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(71, 12);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "*确认密码：";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(21, 148);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(119, 12);
+			this.label4.TabIndex = 6;
+			this.label4.Text = "请选择需要绑定的U盘";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(23, 163);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(199, 20);
+			this.comboBox1.TabIndex = 7;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(70, 87);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(152, 21);
+			this.textBox4.TabIndex = 9;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(27, 90);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(47, 12);
+			this.label5.TabIndex = 8;
+			this.label5.Text = "*邮箱：";
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(70, 114);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(152, 21);
+			this.textBox5.TabIndex = 11;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(33, 117);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(41, 12);
+			this.label6.TabIndex = 10;
+			this.label6.Text = "手机：";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(23, 212);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 12;
+			this.button1.Text = "注册";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(147, 212);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 13;
+			this.button2.Text = "取消";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(36, 192);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(95, 12);
+			this.label7.TabIndex = 14;
+			this.label7.Text = "其中带*为必填项";
 			// 
 			// RegistryForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.ClientSize = new System.Drawing.Size(247, 245);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox1);
 			this.Controls.Add(this.label1);
 			this.Name = "RegistryForm";
-			this.Text = "RegistryForm";
+			this.Text = "注册";
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -56,5 +200,19 @@
 		#endregion
 
 		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label7;
 	}
 }
